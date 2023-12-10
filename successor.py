268e190686258b0827b771464145675318fb0276
Paria Razavi <paria.razavi29@gmail.com>
2023-12-11 02:06:35 +0330

    Added Direction Enum Class
    Added Direction Enum Class

--- successor.py
+++ successor.py
@@ -1,58 +1,96 @@
 import re
+from enum import Enum
+
+class Direction(Enum):
+    """
+    Direction Enum for Top, Down, Left and Right.
+    """
+    Up = (-1, 0)
+    Down = (1, 0)
+    Left = (0, -1)
+    Right = (0, 1)
 
 class Node:
-    def __init__(self, parents, cell_cost, position, remaining_energy):
+    def __init__(self, parents, cell_cost, position, remaining_energy,type):
         self.parents = parents
         self.cell_cost = cell_cost
         self.position = position
         self.remaining_energy = remaining_energy
-
-
-#getting the input
-x, y = map(int, input().split())
-matrix = []
-#preparing the matrix
-for i in range(x):
-    value = list(input().split())
-    matrix.append(value)
-#our default energy
-energy = 500
-
-#our matrix value might be combination of letters and digits so we might need this to parse it
-def extract_digit_and_word(string):
-    match = re.match(r"(\d+)(\w+)", string)
+        self.direction = None # Not set yet
+        self.type = type
     
+    def set_direction(self, direction: Direction):
+        self.direction = direction
+
+
+def get_input():
+    """
+    Function to get the input from the user
+    """
+
+    targetN =0
+    x, y = map(int, input().split())
+    input_matrix = []
+    # preparing the matrix
+    for i in range(x):
+        value = list(input().split())
+        for i in value:
+            if 'T' in i:
+                targenN +=1
+        input_matrix.append(value)
+    # our default energy
+    energy = 500
+    return input_matrix, energy ,targetN
+
+
+def extract_digit_and_word(string):
+    """
+    our matrix value might be combination of letters and digits so we might need this to parse it
+    """
+    match = re.match(r"(\d+)(\w+)", string)
+
     if match:
         digit_part = match.group(1)
         word_part = match.group(2)
         return digit_part, word_part
     else:
         return None, None
-    
-#find our starting point to traverse
+
+
 def find_starting_point(grid, energy):
+    """
+    find our starting point to traverse
+    """
     for row_idx, row in enumerate(grid):
         for col_idx, cell_value in enumerate(row):
             digit, word = extract_digit_and_word(cell_value)
             if word == 'R':
                 energy -= int(digit)
                 position = (row_idx, col_idx)
-                return Node([], 0, position, energy)
-            
+                return Node([], 0, position, energy,'R')
+
     return None  # Return None if starting point not found
 
 
 def successor(current_state, grid):
+    """
+    A function to get the children/successors of a current state given the grid
+    """
     successors = []
-    moves = [(0, 1), (0, -1), (1, 0), (-1, 0)]  # Possible moves: right, left, down, up
+
+    moves = []  # Possible moves: right, left, down, up
+    for name, obj in Direction.__members__.items():
+        moves.append(obj.value)
+
     position = current_state.position
     current_energy = current_state.remaining_energy
     current_row, current_col = position
+    type = None
 
     for move in moves:
         new_row = current_row + move[0]
         new_col = current_col + move[1]
-        
+
         # Check if the new position is within the grid boundaries
         if 0 <= new_row < len(grid) and 0 <= new_col < len(grid[0]):
             new_position = (new_row, new_col)
@@ -61,10 +99,10 @@ def successor(current_state, grid):
                 new_energy = current_energy
                 if cell_value.isdigit():  # Check if the cell value is a digit (cost)
                     cell_cost = int(cell_value)
-                
+
                     if cell_cost <= current_energy:  # Check if the energy is sufficient to move to the new position
                         new_energy = current_energy - cell_cost
-                        successor_node = Node(current_state, cell_cost, new_position, new_energy)
+                        successor_node = Node(current_state, cell_cost, new_position, new_energy,type)
                         successors.append(successor_node)
                 else:
                     reward = 0
@@ -75,24 +113,17 @@ def successor(current_state, grid):
                         reward = 5
                     elif word == 'I':  # Reward: I gives 12 energy
                         reward = 12
-                    elif word == 'T' or word == 'R':  # Target and start position, no energy change
+                    elif word == 'T' :  # Target and start position, no energy change
                         reward = 0
-                    
+                        type = 'T'
+                    elif word == 'R':
+                        reward =0
+
                     cell_cost = int(digit) - reward
-                    
+
                     if cell_cost <= current_energy:  # Check if the energy is sufficient to move to the new position
                         new_energy = current_energy - cell_cost
-                        successor_node = Node(current_state, cell_cost, new_position, new_energy)
+                        successor_node = Node(current_state, cell_cost, new_position, new_energy,type)
                         successors.append(successor_node)
 
     return successors
-
-start = find_starting_point(matrix, energy)
-successors = successor(start, matrix)
-
-for s in successors:
-    print("Parents:", s.parents.position if s.parents else "None")
-    print("Cell Cost:", s.cell_cost)
-    print("Position:", s.position)
-    print("Remaining Energy:", s.remaining_energy)
-    print()
